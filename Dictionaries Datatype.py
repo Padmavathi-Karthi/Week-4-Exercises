1feeead12c14c93e509899c52d718ccdac36066b
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-19 16:59:10 +0100

    update method added

--- Dictionaries Datatype.py
+++ Dictionaries Datatype.py
@@ -76,3 +76,20 @@ print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":1
 # A Lamborghini Diablo costs 55000 dollars
 # A Ford Fiesta costs 13000 dollars
 # A Toyota Prius costs 24000 dollars
+
+print(" ")
+
+# 5. Tessa and Rick are hosting a party. Both sent out invitations to their friends, and each one collected responses into dictionaries, with names of their friends and how many guests each friend was bringing. Each dictionary is a partial guest list, but Rick's guest list has more current information about the number of guests. 
+
+# Complete the function to combine both dictionaries into one, with each friend listed only once, and the number of guests from Rick's dictionary taking precedence, if a name is included in both dictionaries. Then print the resulting dictionary.
+
+def combine_guests(guests1, guests2):
+  guests2.update (guests1) # Use a dictionary method to combine the dictionaries.
+  return guests2
+
+Ricks_guests = { "Adam":2, "Camila":3, "David":1, "Jamal":3, "Charley":2, "Titus":1, "Raj":4}
+Tessas_guests = { "David":4, "Noemi":1, "Raj":2, "Adam":1, "Sakira":3, "Chidi":5}
+
+print(combine_guests(Ricks_guests, Tessas_guests))
+# Should print:
+# {'David': 1, 'Noemi': 1, 'Raj': 4, 'Adam': 2, 'Sakira': 3, 'Chidi': 5, 'Camila': 3, 'Jamal': 3, 'Charley': 2, 'Titus': 1}
