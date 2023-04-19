d37c4fa3f61c7815cdf792b434339c3b58e753bc
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-19 16:28:54 +0100

    another script added

--- Dictionaries Datatype.py
+++ Dictionaries Datatype.py
@@ -8,3 +8,29 @@ def email_list(domains):
 	return(emails)
 
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+print(" ")
+
+# 2. The groups_per_user function receives a dictionary, which contains group names with the list of users. Users can belong to multiple groups. Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values.
+
+def groups_per_user(group_dictionary):
+	user_groups = {}
+	# Go through group_dictionary
+	for group_names, users in group_dictionary.items():
+		# Now go through the users in the group
+		for user in users:
+			if user in user_groups:
+				user_groups[user].append(group_names)
+			else:
+				user_groups[user] = [group_names]
+				
+				
+			# Now add the group to the the list of
+# groups for this user, creating the entry
+# in the dictionary if necessary
+
+	return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
